aef837155fecf9dadf84f728e69c5561a503344d
joeyorjohn <50225065+joeyorjohn@users.noreply.github.com>
2021-06-09 20:29:08 +1000

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
 import "../styles/globals.css";
-import { AppWrapper } from "../context/state"; // import based on where you put it
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <AppWrapper>
-      <Component {...pageProps} />;
-    </AppWrapper>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
